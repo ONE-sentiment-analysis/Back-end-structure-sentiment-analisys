fae13f692b11c91ef637efba37fdb055e1191a8d
Cauan Henrique <cauanhenrique56@gmail.com>
2025-12-22 12:08:39 -0300

    Update SentimentController, SentimentItemResponse, IdReferencia - Exclusão dos comentários, exclusão da redundância @AtrributeOverride das variáveis idReferencia e TextoAvaliacao, adição da data de processamento na resposta da requisição e adição do construtor da classe IdReferencia no construtor da classe AnaliseSentimento

--- src/main/java/br/com/one/sentiment_analysis/dto/response/SentimentItemResponse.java
+++ src/main/java/br/com/one/sentiment_analysis/dto/response/SentimentItemResponse.java
@@ -1,8 +1,11 @@
 package br.com.one.sentiment_analysis.dto.response;
 
+import java.time.LocalDateTime;
+
 public record SentimentItemResponse(
         String idReferencia,
         String texto,
         String previsao,
-        double probabilidade
+        double probabilidade,
+        LocalDateTime dataProcessamento
 ) {}
--- src/main/java/br/com/one/sentiment_analysis/model/IdReferencia.java
+++ src/main/java/br/com/one/sentiment_analysis/model/IdReferencia.java
@@ -13,8 +13,6 @@ import java.util.regex.Pattern;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA requires this
 public class IdReferencia implements Serializable {
-
-    // Compilamos o padrão apenas uma vez para performance (static final)
     private static final String REGEX_PADRAO = "^prod_\\d+_review_\\d+$";
     private static final Pattern PATTERN = Pattern.compile(REGEX_PADRAO);
 
@@ -26,7 +24,6 @@ public class IdReferencia implements Serializable {
             throw new IllegalArgumentException("O ID de referência é obrigatório.");
         }
 
-        // Validação do Regex
         if (!PATTERN.matcher(valor).matches()) {
             throw new IllegalArgumentException("O ID '" + valor + "' está fora do padrão esperado (ex: prod_123_review_456).");
         }
@@ -43,13 +40,12 @@ public class IdReferencia implements Serializable {
         }
     }
 
-    // Facilita o uso do objeto como String normal quando necessário
+
     @Override
     public String toString() {
         return valor;
     }
 
-    // Importante implementar equals/hashCode para Value Objects
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
