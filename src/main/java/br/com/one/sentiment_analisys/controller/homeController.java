2934a733875f536dc4a33910185a87c587f139f9
pablo <stevaopabllo@gmail.com>
2025-12-22 18:56:52 -0300

    feat: added swagger into project

--- src/main/java/br/com/one/sentiment_analisys/controller/homeController.java
+++ src/main/java/br/com/one/sentiment_analisys/controller/homeController.java
@@ -1,21 +1,39 @@
 package br.com.one.sentiment_analisys.controller;
 
-import lombok.Getter;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Rotas de testes do Swagger", description = "Endpoints para verificar se está configurado e funcionando tudo corretamente")
 public class homeController {
 
-    @GetMapping()
-    public String helloSpring(){
-        return "Olá, Spring boot";
-    };
+    @GetMapping
+    @Operation(summary = "Retorna um Olá, Spring Boot")
+    @ApiResponse(responseCode = "200", description = "GET funcionando")
+    @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    public ResponseEntity<String> helloSpring() {
+        return ResponseEntity.ok("Olá, Spring Boot");
+    }
+
+
 
     @GetMapping("/hello/{name}")
-    public String helloName(@PathVariable String name) {
-        return "Olá, "+ name.toUpperCase().trim();
+    @Operation(summary = "Retorna uma Saudação para o Usuário", description = "Preencha campo Nome")
+    @ApiResponse(
+        responseCode = "200",
+        description = "GET saudação funcionando",
+        content = @Content(mediaType = "application/json",
+            examples = @ExampleObject(value = "{ \"mensagem\": \"Olá, Papai Noel\" }"))
+    )
+    public ResponseEntity<String> helloName(@PathVariable String name) {
+        return ResponseEntity.ok("Olá, " + name.toUpperCase().trim());
     }
 }
