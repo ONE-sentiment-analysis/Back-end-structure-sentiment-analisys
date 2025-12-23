cc5e54d4215c7910a7582d83dad5341f2c666b1e
pablo <stevaopabllo@gmail.com>
2025-12-23 16:33:13 -0300

    fixed: update sentiment controller swagger

--- src/main/java/br/com/one/sentiment_analisys/controller/SentimentController.java
+++ src/main/java/br/com/one/sentiment_analisys/controller/SentimentController.java
@@ -3,6 +3,11 @@ package br.com.one.sentiment_analisys.controller;
 import br.com.one.sentiment_analisys.model.SentimentRequestModel;
 import br.com.one.sentiment_analisys.model.SentimentResponse;
 import br.com.one.sentiment_analisys.service.ExternalApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/sentiment")
+@RequestMapping("/api/v1/sentiment")
+@Tag(name = "Endpoint para realizar análise de sentimentos", description = "Retorna probabilidade e acurácia do comentário")
 public class SentimentController {
 
     private final ExternalApiService sentimentService;
@@ -21,7 +27,10 @@ public class SentimentController {
     }
 
 
-    @PostMapping
+    @PostMapping @Operation(summary = "Analisar comentário", description = "Recebe um texto e retorna análise de sentimento")
+    @ApiResponse( responseCode = "200", description = "Comentário analisado com sucesso",
+            content = @Content( mediaType = "application/json",
+                    examples = @ExampleObject( value = "{ \"previsao\": \"positivo\", \"probabilidade\": 0.94 }" ) ) )
     public SentimentResponse analisarComentario (@RequestBody SentimentRequestModel text)
             throws IOException, InterruptedException {
 
