58f67813de81179d0ca843c15bee7572c53840c3
pablo <stevaopabllo@gmail.com>
2025-12-22 22:15:12 -0300

    feat: added spring security into project

--- src/main/java/br/com/one/sentiment_analisys/config/SecurityConfig.java
+++ src/main/java/br/com/one/sentiment_analisys/config/SecurityConfig.java
@@ -1,6 +1,25 @@
 package br.com.one.sentiment_analisys.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     // TODO adicionar spring security e criar função 
+      @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/**").permitAll() // Qualquer pessoa tem acesso a rota que tenha esse prefixo
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
 }
--- src/main/java/br/com/one/sentiment_analisys/controller/homeController.java
+++ src/main/java/br/com/one/sentiment_analisys/controller/homeController.java
@@ -3,6 +3,7 @@ package br.com.one.sentiment_analisys.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@RequestMapping("/api/v1")
 @Tag(name = "Rotas de testes do Swagger", description = "Endpoints para verificar se está configurado e funcionando tudo corretamente")
 public class homeController {
 
