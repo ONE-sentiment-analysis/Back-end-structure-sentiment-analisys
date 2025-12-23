35580417f5d140143964d49479dba5599988631d
pablo <stevaopabllo@gmail.com>
2025-12-23 15:48:47 -0300

    build: update ci/cd pipeline

--- src/test/java/br/com/one/sentiment_analysis/SentimentAnalisysApplicationTests.java
+++ src/test/java/br/com/one/sentiment_analysis/SentimentAnalisysApplicationTests.java
@@ -3,7 +3,7 @@ package br.com.one.sentiment_analysis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SentimentAnalysisApplicationTests.class)
 class SentimentAnalysisApplicationTests {
 
 	@Test
