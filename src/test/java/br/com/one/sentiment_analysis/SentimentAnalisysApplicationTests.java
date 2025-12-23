b25ace89ca17795e39bd976b920509505b817d51
pablo <stevaopabllo@gmail.com>
2025-12-23 15:41:27 -0300

    test: updated class test

--- src/test/java/br/com/one/sentiment_analysis/SentimentAnalisysApplicationTests.java
+++ src/test/java/br/com/one/sentiment_analysis/SentimentAnalisysApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SentimentAnalisysApplicationTests {
+class SentimentAnalysisApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
