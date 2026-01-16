2087a0ced4632bf3a6568c725678b4e60fa38e6f
Cauan Henrique <cauanhenrique56@gmail.com>
2026-01-16 02:40:15 -0300

    Hotfix - mudança de banco de dados e atualização da classe SecurityConfig para integração com o front-end

--- src/main/java/br/com/one/sentiment_analysis/config/SecurityConfig.java
+++ src/main/java/br/com/one/sentiment_analysis/config/SecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -76,6 +78,20 @@ public class SecurityConfig {
             .build();
 
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8501")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
     @Bean
